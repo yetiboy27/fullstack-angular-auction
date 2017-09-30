4cf63864d2bbaaef93487a967a90d7ad8b3a28b4
Tim Osborn <yetiboy@earthlink.net>
2017-09-30 15:36:31 -0600

    start over on auction site

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,24 +1,10 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import ApplicationComponent from './components/application/application';
-import CarouselComponent from "./components/carousel/carousel";
-import FooterComponent from "./components/footer/footer";
-import NavbarComponent from "./components/navbar/navbar";
-import ProductItemComponent from "./components/product-item/product-item";
-import SearchComponent from "./components/search/search";
-import StarsComponent from "./components/stars/stars";
-import {ProductService} from "./services/product-service";
+import { AppComponent }  from './app.component';
 
 @NgModule({
     imports:      [ BrowserModule ],
-    declarations: [ ApplicationComponent,
-                    CarouselComponent,
-                    FooterComponent,
-                    NavbarComponent,
-                    ProductItemComponent,
-                    SearchComponent,
-                    StarsComponent],
-    providers:    [ProductService],
-    bootstrap:    [ ApplicationComponent ]
+    declarations: [ AppComponent ],
+    bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
