cb8a068841991b8d9c2cf3162860bbc23ba705de
Tim Osborn <yetiboy@earthlink.net>
2017-10-01 13:54:53 -0600

    updated package.json so that all modules load

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from "@angular/router";
-import { LocationStrategy, HashLocationStrategy } from "@angular/common";
+import { RouterModule } from '@angular/router';
+import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 import ApplicationComponent from './components/application/application';
-import CarouselComponent from './components/carousel/carousel';
-import FooterComponent from './components/footer/footer';
-import NavbarComponent from './components/navbar/navbar';
-import ProductItemComponent from './components/product-item/product-item';
-import SearchComponent from './components/search/search';
-import StarsComponent from './components/stars/stars';
+import CarouselComponent from "./components/carousel/carousel";
+import FooterComponent from "./components/footer/footer";
+import NavbarComponent from "./components/navbar/navbar";
+import ProductItemComponent from "./components/product-item/product-item";
+import SearchComponent from "./components/search/search";
+import StarsComponent from "./components/stars/stars";
 import {ProductService} from "./services/product-service";
 import HomeComponent from "./components/home/home";
 import ProductDetailComponent from "./components/product-detail/product-detail";
@@ -16,12 +16,9 @@ import ProductDetailComponent from "./components/product-detail/product-detail";
 @NgModule({
     imports:      [ BrowserModule,
                     RouterModule.forRoot([
-                        {path: '',
-                        component: HomeComponent},
-                        {path: 'products/:prodTitle',
-                        component: ProductDetailComponent}
-
-                    ]) ],
+                        {path: '',                    component: HomeComponent},
+                        {path: 'products/:prodTitle', component: ProductDetailComponent}
+    ]) ],
     declarations: [ ApplicationComponent,
                     CarouselComponent,
                     FooterComponent,
@@ -31,8 +28,8 @@ import ProductDetailComponent from "./components/product-detail/product-detail";
                     ProductItemComponent,
                     SearchComponent,
                     StarsComponent],
-    providers:      [ProductService,
-                    { provide: LocationStrategy, useClass: HashLocationStrategy }],
+    providers:    [ProductService,
+                   {provide: LocationStrategy, useClass: HashLocationStrategy}],
     bootstrap:    [ ApplicationComponent ]
 })
 export class AppModule { }
